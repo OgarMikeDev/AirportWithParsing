30a88c94ae0cb542140651bc5155c2b7864b2622
Mike Ogar <ogar.m.v@mail.ru>
2025-06-10 17:25:39 +0300

    airport

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,29 +9,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String user = "root";
-        String url = "jdbc:mysql://localhost:3306/aviasales";
-        String password = "Misha06122000)))";
-
-        try {
-            Connection connection = DriverManager.getConnection(url, user, password);
-            Statement statement = connection.createStatement();
-
-            Airport airport = new Airport();
-            airport.getListAllAirports();
-
-            for (Map.Entry<String, String> entryAirport : airport.getMapAllAirports().entrySet()) {
-                statement.execute("INSERT INTO airports (name, url) VALUES (" +
-                        entryAirport.getKey() + "," + entryAirport.getValue() + ")");
-            }
-
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            Bot bot = new Bot();
-            telegramBotsApi.registerBot(bot);
-
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        Bot bot = new Bot();
+        telegramBotsApi.registerBot(bot);
     }
 }
